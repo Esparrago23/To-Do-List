08dc8d280005ab43d8208af9a2214f17c7ad53f2
Esparrago23 <minisirhc@gmail.com>
2025-10-21 19:31:37 -0600

    Front

--- frontend/Christian-CATH-to-do-List/src/app/app.ts
+++ frontend/Christian-CATH-to-do-List/src/app/app.ts
@@ -1,12 +1,61 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms';
+import { TareaService } from './tarea.service';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('Christian-CATH-to-do-List');
+
+
+  public mensajeDelApi: string = 'Cargando...';
+  public tareas: any[] = [];
+  public nuevaTareaDescripcion: string = '';
+
+  constructor(private tareaService: TareaService) {}
+
+  ngOnInit(): void {
+    this.cargarMensajePersonal();
+    this.cargarTareas();
+  }
+
+  cargarMensajePersonal(): void {
+    this.tareaService.getNombreCompleto().subscribe(data => {
+      this.mensajeDelApi = data.nombreCompleto;
+    });
+  }
+
+  cargarTareas(): void {
+    this.tareaService.getTareas().subscribe(data => {
+      this.tareas = data;
+    });
+  }
+
+  agregarTarea(): void {
+    if (this.nuevaTareaDescripcion.trim() === '') {
+      return; 
+    }
+
+    this.tareaService.addTarea(this.nuevaTareaDescripcion).subscribe(() => {
+      this.cargarTareas(); 
+      this.nuevaTareaDescripcion = ''; 
+    });
+  }
+
+  toggleTarea(id: number): void {
+    this.tareaService.toggleTarea(id).subscribe(() => {
+      this.cargarTareas(); 
+    });
+  }
+
+  eliminarTarea(id: number): void {
+    this.tareaService.deleteTarea(id).subscribe(() => {
+      this.cargarTareas(); 
+    });
+  }
 }
---
+++ frontend/Christian-CATH-to-do-List/src/app/tarea.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TareaService {
+
+  private apiUrl = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) { }
+
+
+  getNombreCompleto(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/Torres`);
+  }
+
+
+
+  getTareas(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/tareas`);
+  }
+
+
+  addTarea(descripcion: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/tareas`, { descripcion });
+  }
+
+
+  toggleTarea(id: number): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/tareas/${id}`, {});
+  }
+
+
+  deleteTarea(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/tareas/${id}`);
+  }
+}
